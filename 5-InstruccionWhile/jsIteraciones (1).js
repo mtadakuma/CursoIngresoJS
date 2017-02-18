b4819c9e34f3ca2f2c737b4a2dbe1b0a1046aa1b
alumno <alumno>
2017-02-17 22:07:32 -0300

    clase 6

--- 5-InstruccionWhile/jsIteraciones (1).js
+++ 5-InstruccionWhile/jsIteraciones (1).js
@@ -1,10 +1,42 @@
 function Mostrar()
 {
-	alert('iteración while');
+/*	alert('iteración while');
 	var num = 1;
 	while(num < 11){
 		console.log(num);
 		++num;
 	}
+*/
+var nota;
+var contAlumnos;
+var contAprob;
+var contDiez;
+var acumNotas;
+var acumAprob;
+var sexo;
+var cantMasc;
+var cantFem;
+
+while(confirm("¿Desea continuar?")){
+	nota = parseInt(prompt("Ingrese nota"));
+	sexo = prompt("Ingrese sexo");
+	contAlumnos++;
+	acumNotas+= nota;
+	if(nota > 4){
+		acumAprob+= nota;
+		contAprob ++;
+	}
+	if (nota == 10){
+		contDiez++;
+	}
+	if (sexo != "F"){
+		cantMasc++;
+	} else {
+		cantFem++;
+	}
+
+
+
+}
 
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -42,6 +42,7 @@ console.log( " Positivos: " + positivos,
 	);
 
 }
+
 /*
 function Mostrar()
 {
